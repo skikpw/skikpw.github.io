12602d50815e065ef8e78aa6efdf58b376a84c06
wasilewski94 <wasilkris@gmail.com>
2017-05-12 15:00:35 +0000

    delete

--- commit_and_push.sh
+++ commit_and_push.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-echo commit i push pliku temp.py
-git add temp.py
-git commit -m "upload pliku index.html"
+echo commit i push pliku index.html
+echo $1
+git add . 
+git commit -m "$1"
 git push origin master
 
--- temp.py
+++ temp.py
@@ -6,34 +6,14 @@ os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
 
 base_dir = '/sys/bus/w1/devices/'
-device_folder1 = glob.glob(base_dir + '28-00*')[0]
+device_folder1 = glob.glob(base_dir + '10-00*')[0]
 device_file1 = device_folder1 + '/w1_slave'
-
-#device_folder2 = glob.glob(base_dir + '28-03168*')[0]
-#device_file2 = device_folder2 + '/w1_slave' 
-
-#device_folder3 = glob.glob(base_dir + '28-04168*')[0]
-#device_file3 = device_folder3 + '/w1_slave' 
-
 
 def read_temp_raw1():
     f = open(device_file1, 'r')
     lines = f.readlines()
     f.close()
     return lines
-
-#def read_temp_raw2():
- #   f = open(device_file2, 'r')
-  #  lines = f.readlines()
-   # f.close()
-    #return lines
-
-#def read_temp_raw3():
- #   f = open(device_file3, 'r')
-  #  lines = f.readlines()
-   # f.close()
-    #return lines
-
 
 def read_temp1():
     lines = read_temp_raw1()
@@ -45,29 +25,6 @@ def read_temp1():
         temp_string = lines[1][equals_pos+2:]
         temp_c = float(temp_string) / 1000.0
         return temp_c
-
-
-#def read_temp2():
- #   lines = read_temp_raw2()
-  #  while lines[0].strip()[-3:] != 'YES':
-   #     time.sleep(0.2)
-    #    lines = read_temp_raw2()
-    #equals_pos = lines[1].find('t=')
-    #if equals_pos != -1:
-     #   temp_string = lines[1][equals_pos+2:]
-      #  temp_c = float(temp_string) / 1000.0
-       # return temp_c
-
-#def read_temp3():
- #   lines = read_temp_raw3()
-  #  while lines[0].strip()[-3:] != 'YES':
-   #     time.sleep(0.2)
-    #    lines = read_temp_raw3()
-    #equals_pos = lines[1].find('t=')
-    #if equals_pos != -1:
-     #   temp_string = lines[1][equals_pos+2:]
-      #  temp_c = float(temp_string) / 1000.0
-       # return temp_c
 
 main_str = """
         <html>
@@ -102,24 +59,18 @@ cold_str = """
         <br>
         """
 
-plik_html = open("/home/pi/strona/skikpw.github.io/index.html", "w")
+plik_html = open("/home/skik/skikpw.github.io/index.html", "w")
 plik_html.write(main_str)
 plik_html.close()
 temperature = 0.0
 
 licznik=0
 while True:
-	licznik+=1
-        plik_html = open("/home/pi/strona/skikpw.github.io/index.html","a+")
+        licznik+=1
+        plik_html = open("/home/skik/skikpw.github.io/index.html", "a+")
         temperature1 = read_temp1()
-#        temperature2 = read_temp2()
- #       temperature3 = read_temp3()
         plik_html.write(str(temperature1))
         plik_html.write("<br>")
-  #      plik_html.write(str(temperature2))
-   #     plik_html.write("<br>")
-    #    plik_html.write(str(temperature3))
-     #   plik_html.write("<br>")
 
         if temperature1 < 24:
                 plik_html.write(cold_str)
@@ -131,12 +82,10 @@ while True:
                 plik_html.write(norm_str)
 
         print(str(temperature1))
-        #print(str(temperature2))       
-        #print(str(temperature3))       
         print(" ")
         plik_html.close()
-	if licznik>=20:
-		break
+        if licznik>=20:
+                break
 
 
 
